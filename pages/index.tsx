113658aa34d61144f4810ca12a68e45b96a06f67
Lucas-Amaral <lukinhasilvaamaral@gmail.com>
2022-11-23 10:26:27 -0300

    :tada: First commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Text color={'primary.main'}>
+      APROVEITE as promoções DE VIAGEM PARA CONHECER O BRASIL
+    </Text>
+  )
+}
---
+++ src/common/theme/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  black: '#000000',
+  white: '#FFFFFF',
+  primary: {
+    dark: '#a2c9ff',
+    main: '#008DF3',
+    light: '#0060a9',
+  },
+  secundary: {
+    dark: '#f4c000',
+    main: '#F8C301',
+    light: '#755b00',
+  },
+  error: {
+    dark: '#ffb4ab',
+    light: '#ba1a1a',
+  },
+  background: {
+    dark: '#1a1c1e',
+    light: '#fdfcff',
+  },
+}
